27ef85ea754c848ec66500d14e21c0b0b6ec1d0f
Merieme Bouisri <meriemebouisri@gmail.com>
2023-05-28 00:25:56 -0400

    Add convex hull function

---
+++ draw_utils.py
@@ -0,0 +1,127 @@
+import cv2
+import numpy as np
+
+import math_utils
+
+DEFAULT_COLOR = (0, 0, 255)
+DEFAULT_THICKNESS = 1
+
+
+def draw_cross(frame, point, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=2):
+    """
+    Draw a cross on the frame at the given point.
+    """
+    x, y, size = int(point[0]), int(point[1]), int(length / 2)
+    cv2.line(frame, (x - size, y), (x + size, y), color, thickness)
+    cv2.line(frame, (x, y - size), (x, y + size), color, thickness)
+
+
+def draw_points(frame, points, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=2):
+    """
+    Draw a cross on the frame at each point in the points list.
+    """
+    for point in points:
+        draw_cross(frame, point, color=color, thickness=thickness, length=length)
+
+def draw_line(frame, point1, point2, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a line on the frame joining the two points.
+    Helper function for not forgetting to convert the coordinates to integers.
+    """
+    pt1 = (int(point1[0]), int(point1[1]))
+    pt2 = (int(point2[0]), int(point2[1]))
+    cv2.line(frame, pt1, pt2, color, thickness)
+
+def draw_equation_line(frame, domain_range, linear_equation: callable, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a line on the frame according to the given linear equation, and within the domain range of the equation.
+
+    @param frame: The image on which to draw the line
+    @param domain_range: A tuple containing the domain of the function
+    @param linear_equation: A callable that takes a number as input and returns a number as output
+    """
+
+    point1 = domain_range[0], linear_equation(domain_range[0])
+    point2 = domain_range[1], linear_equation(domain_range[1])
+
+    draw_line(frame, point1, point2, color, thickness)
+
+
+def draw_joined_segments(frame, points, closed=True, color=DEFAULT_COLOR, width=2, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a line joining each point in the points list to the next one in the list, with the last point joined to the first one by default.
+    See draw_polygon() for non-intersecting lines.
+    """
+
+    for i in range(len(points) - 1):
+        draw_line(frame, points[i], points[i + 1], color, thickness)
+
+    if closed:
+        draw_line(frame, points[0], points[-1], color, thickness)
+
+
+def draw_polygon(frame, vertices, closed=True,  color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a polygon on the frame by joining the vertices in the points list such as the lines joining each vertex to the next one in the list do not intersect.
+    """
+    sorted_vertices = math_utils.get_non_intersecting_vertices(vertices)
+    draw_joined_segments(frame, sorted_vertices, closed=closed, color=color, thickness=thickness)
+
+def draw_convex_hull(frame, vertices, closed=True, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a convex polygon on the frame by joining the vertices in the points list.
+    """
+    sorted_vertices = math_utils.get_convex_hull(vertices)
+    draw_polygon(frame, sorted_vertices, closed=closed, color=color, thickness=thickness)
+
+
+
+
+
+def draw_min_enclosing_rectangle(frame, vertices, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
+    """
+    Draw a rectangle on the frame that encloses the given vertices.
+    Returns the coordinates of the top left and bottom right vertices of the rectangle.
+    """
+    # Get the coordinates of the top left corner
+    x_min = int(min(vertices, key=lambda x: x[0])[0])
+    y_min = int(min(vertices, key=lambda x: x[1])[1])
+
+    # Get the coordinates of the bottom right corner
+    x_max = int(max(vertices, key=lambda x: x[0])[0])
+    y_max = int(max(vertices, key=lambda x: x[1])[1])
+
+    # Draw the rectangle
+    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, thickness)
+
+    return (x_min, y_min), (x_max, y_max)
+    
+# ------------------------------
+# EXAMPLE 
+# ------------------------------
+if __name__ == '__main__':
+
+    
+    frame = np.zeros((500, 500, 3), dtype=np.uint8)
+
+    # random points
+    vertices = []
+
+    for i in range(100):
+        x = np.random.randint(0, 500)
+        y = np.random.randint(0, 500)
+
+        vertices.append((x, y))
+
+    draw_points(frame, vertices, length=5)
+
+    draw_convex_hull(frame, vertices, color=(0, 255, 0))
+
+
+    while True:
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):  
+            break
+
+    
---
+++ math_utils.py
@@ -0,0 +1,122 @@
+import numpy as np
+import math
+import draw_utils
+
+def calculate_slope(point1, point2):
+    """
+    Calculates the slope of line intersecting the two points.
+    Returns math.inf if the line is vertical (division by zero)
+    """
+
+    if (point2[0] - point1[0]) == 0: return math.inf
+    return (point2[1] - point1[1]) / (point2[0] - point1[0])
+
+
+
+def calculate_intercept(slope, point):
+    """
+    Calculates the intercept of the line with the given slope and the given point
+    """
+    return point[1] - slope * point[0]
+
+
+def get_linear_equation(point1, point2):
+    """
+    Returns a callable that represents the linear equation of the line intersecting the two points.
+    """
+    slope = calculate_slope(point1, point2)
+    intercept = calculate_intercept(slope, point1)
+    
+    return lambda x: slope * x + intercept
+
+def get_non_intersecting_vertices(vertices):
+    # Sort the vertices by their x coordinate in ascending order
+    sorted_vertices = sorted(vertices, key=lambda x: x[0])
+
+    # Get the leftmost and rightmost points
+    leftmost_vertex = sorted_vertices[0]
+    rightmost_vertex = sorted_vertices[-1]
+
+    # Get the equation of the line joining the leftmost and rightmost points
+    equation = get_linear_equation(leftmost_vertex, rightmost_vertex)
+
+    # Separate each point in two lists: one for the points above the line, and one for the points below the line
+    points_above_line, points_below_line = [], []
+    for vertex in sorted_vertices:
+        points_above_line.append(vertex) if vertex[1] < equation(vertex[0]) else points_below_line.append(vertex)
+
+    points_above_line.sort(key=lambda x: x[0])
+    points_below_line.sort(key=lambda x: x[0], reverse=True)
+
+    # Concatenate the two lists
+    return points_above_line + points_below_line 
+
+def get_convex_hull(vertices):
+    convex_hull = []
+    sorted_vertices = sorted(vertices, key=lambda x: [x[0], x[1]])
+    
+    start = sorted_vertices.pop(0)
+    convex_hull.append(start)
+    sorted_vertices = sorted(sorted_vertices, key=lambda p: (calculate_slope(start, p), -p[1], p[0]))
+
+    for vertex in sorted_vertices:
+        convex_hull.append(vertex)
+        while len(convex_hull) > 2 and get_cross_product(convex_hull[-3], convex_hull[-2], convex_hull[-1]) < 0:
+            convex_hull.pop(-2)
+ 
+    return convex_hull
+
+
+
+    
+
+
+
+def calculate_angle_degrees(point1, point2):
+    """
+    Returns angle (in degrees) of the vector intersecting the two points.
+    """
+    return np.arctan(calculate_slope(point1, point2)) * 180 / np.pi
+
+
+def calculate_magnitude(point1, point2):
+    """
+    Calculated the magnitude of a vector intersecting the two points.
+    """
+    x1, y1 = point1
+    x2, y2 = point2
+    return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def get_cross_product(point1, point2, point3):
+    """
+    Returns the cross product of the two vectors intersecting the two points.
+    """
+    return ((point2[0] - point1[0]) * (point3[1] - point1[1])) - ((point2[1] - point1[1]) * (point3[0] - point1[0]))
+
+
+def as_geometric_vector(initial_point, terminal_point):
+    """
+    A vector representation of a line that starts at the initial point and ends at the terminal point. 
+    Returns a tuple containing the vector's magnitude and angle with respect to the horizontal axis.
+    """
+    return calculate_magnitude(initial_point, terminal_point), calculate_angle_degrees(initial_point, terminal_point)
+
+
+
+def map_by_column(list, callables):
+    """
+    Maps the given list by the given callables. The callable of index n is applied to nth element of each column of the list.
+    Example :
+    list = [[1,2,3], [4,5,6], [7,8,9]]
+    callables = [lambda x: x * 1, lambda x: x * 0, lambda x: x * 1]
+    map_by_column(list, callables)
+    >> [[1,0,3], [4,0,6], [7,0,9]]
+    """
+    
+    flat_array = np.array(list).flatten()
+    mapping_function = lambda i: callables[i % len(callables)](flat_array[i])
+    flat_array = np.array([mapping_function(i) for i in range(len(flat_array))])
+    return flat_array.reshape(-1, len(list[0]))
+
+
+
