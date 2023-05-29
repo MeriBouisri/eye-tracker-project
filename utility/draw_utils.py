ec27138a11f53acaec844ec9139344d8db7dc520
Merieme Bouisri <meriemebouisri@gmail.com>
2023-05-29 19:31:22 -0400

    Add documentation

--- utility/draw_utils.py
+++ utility/draw_utils.py
@@ -1,18 +1,34 @@
 import cv2
 import numpy as np
 
-import math_utils
+from utility import geometry_utils as geometry
 
 DEFAULT_COLOR = (0, 0, 255)
 DEFAULT_THICKNESS = 1
 DEFAULT_LENGTH = 2
 
-
 def draw_cross(frame, point, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=DEFAULT_LENGTH):
     """
-    Draw a cross on the frame at the given point. A correctly formatted point is subscriptable object
-    who's first two elements are the x and y coordinates of the point, respectively. If incorrectly formatted,
-    the point is converted to a flattened numpy array, and the first two elements are used as the coordinates.
+    Draw a cross on the frame at the given point.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the cross.
+    
+    point : array_like
+        The (x, y) coordinates at which to draw the cross. 
+        If not a subscriptable object, an attempt is made to convert it to a flat numpy array.
+        The first two elements of the array are used as the coordinates.
+    
+    color : tuple, optional
+        The color of the cross. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the lines that make up the cross. The default thickness is 1 px.
+    
+    length : int, optional
+        The length of the lines that make up the cross. The default length is 2 px.
     """
     # Convert the point to a usable format
     point = np.array(point).flatten()
@@ -22,30 +38,94 @@ def draw_cross(frame, point, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, l
     cv2.line(frame, (x, y - size), (x, y + size), color, thickness)
 
 
-def draw_points(frame, points, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=DEFAULT_LENGTH):
+def draw_all_crosses(frame, points, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=DEFAULT_LENGTH):
     """
     Draw a cross on the frame at each point in the points list.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the cross.
+
+    points : array_like
+        The list of (x, y) coordinates at which to draw the crosses. If only one point is given, 
+        the draw_cross() function is called automatically. 
+
+    color : tuple, optional
+        The color of the cross. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the lines that make up the cross. The default thickness is 1 px.
+
+    length : int, optional
+        The length of the lines that make up the cross. The default length is 2 px.
+
+    See Also
+    ----------
+    draw_cross() : Draw a cross at a single point.  
     """
+
+    # Check if the points list is correctly formatted
+    if np.ndim(points) == 1:
+        draw_cross(frame, points, color=color, thickness=thickness, length=length)
+        return
+    
     for point in points:
         draw_cross(frame, point, color=color, thickness=thickness, length=length)
 
-def draw_line(frame, point1, point2, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS, length=DEFAULT_LENGTH):
+
+def draw_line(frame, point1, point2, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a line on the frame joining the two points.
     Helper function for not forgetting to convert the coordinates to integers.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the line.
+    
+    point1 : Any
+        First point of the line segment
+
+    point2 : Any
+        Second point of the line segment
+        
+    color : tuple, optional
+        The color of the line. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the line. The default thickness is 1 px.
+    
+    length : int, optional
+        The length of the line. The default length is 2 px.
     """
     pt1 = (int(point1[0]), int(point1[1]))
     pt2 = (int(point2[0]), int(point2[1]))
     cv2.line(frame, pt1, pt2, color, thickness)
+
 
 def draw_equation_line(frame, domain_range, linear_equation: callable, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a line on the frame according to the given linear equation, and within the domain range of the equation.
     Returns the coordinates of the two points at the extremities of the line.
 
-    @param frame: The image on which to draw the line
-    @param domain_range: A tuple containing the domain of the function
-    @param linear_equation: A callable that takes a number as input and returns a number as output
+    Parameters
+    ----------
+
+    frame : ndarray
+        The image on which to draw the line.
+
+    domain_range : Any
+        A set of two numbers representing the domain of the function described by the linear equation.
+
+    linear_equation : callable
+        A function that takes a number as input and returns a number as output.
+    
+    color : tuple, optional
+        The color of the line. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the line. The default thickness is 1 px.
     """
 
     point1 = domain_range[0], linear_equation(domain_range[0])
@@ -56,10 +136,32 @@ def draw_equation_line(frame, domain_range, linear_equation: callable, color=DEF
     return point1, point2
 
 
-def draw_joined_segments(frame, points, closed=True, color=DEFAULT_COLOR, width=2, thickness=DEFAULT_THICKNESS):
+def draw_joined_segments(frame, points, closed=True, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a line joining each point in the points list to the next one in the list, with the last point joined to the first one by default.
-    See draw_polygon() for non-intersecting lines.
+    
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the line.
+    
+    points : array_like
+        The list of (x, y) coordinates connecting each segment.
+    
+    closed : bool, optional
+        If True, the last point is joined to the first one. Set to True by default.
+    
+    color : tuple, optional
+        The color of the line. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the line. The default thickness is 1 px.
+    
+    See Also
+    ----------
+    draw_line : Draw a line between two points.
+    draw_polygon : Draw non-intersecting segments between the points in the list.
+    draw_convex_hull : Draw non-intersecting segments between the points in the list, such as the segments form a convex polygon.
     """
 
     for i in range(len(points) - 1):
@@ -72,35 +174,92 @@ def draw_joined_segments(frame, points, closed=True, color=DEFAULT_COLOR, width=
 def draw_polygon(frame, vertices, closed=True,  color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a polygon on the frame by joining the vertices in the points list such as the lines joining each vertex to the next one in the list do not intersect.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the line.
+    
+    points : array_like
+        The list of (x, y) coordinates connecting each segment.
+    
+    closed : bool, optional
+        If True, the last point is joined to the first one. Set to True by default.
+    
+    color : tuple, optional
+        The color of the line. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the line. The default thickness is 1 px.
+
+    See Also
+    ----------
+    draw_line : Draw a line between two points.
+    draw_joined_segments : Draw non-intersecting segments between the points in the list.
+    draw_convex_hull : Draw non-intersecting segments between the points in the list, such as the segments form a convex polygon.
     """
-    sorted_vertices = math_utils.get_non_intersecting_vertices(vertices)
+    sorted_vertices = geometry.get_non_intersecting_vertices(vertices)
     draw_joined_segments(frame, sorted_vertices, closed=closed, color=color, thickness=thickness)
+
 
 def draw_convex_hull(frame, vertices, closed=True, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a convex polygon on the frame by joining the vertices in the points list.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the line.
+    
+    points : array_like
+        The list of (x, y) coordinates connecting each segment.
+    
+    closed : bool, optional
+        If True, the last point is joined to the first one. Set to True by default.
+    
+    color : tuple, optional
+        The color of the line. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the line. The default thickness is 1 px.
+    
+    See Also
+    ----------
+    draw_line : Draw a line between two points.
+    draw_joined_segments : Draw non-intersecting segments between the points in the list.
+    draw_polygon : Draw non-intersecting segments between the points in the list.
     """
-    sorted_vertices = math_utils.get_convex_hull(vertices)
+    sorted_vertices = geometry.get_convex_hull(vertices)
     draw_polygon(frame, sorted_vertices, closed=closed, color=color, thickness=thickness)
+
 
 def draw_min_enclosing_rectangle(frame, vertices, color=DEFAULT_COLOR, thickness=DEFAULT_THICKNESS):
     """
     Draw a rectangle on the frame that encloses the given vertices.
-    Returns the coordinates of the top left and bottom right vertices of the rectangle.
-    """
-    # Get the coordinates of the top left corner
-    x_min = int(min(vertices, key=lambda x: x[0])[0])
-    y_min = int(min(vertices, key=lambda x: x[1])[1])
-
-    # Get the coordinates of the bottom right corner
-    x_max = int(max(vertices, key=lambda x: x[0])[0])
-    y_max = int(max(vertices, key=lambda x: x[1])[1])
-
-    # Draw the rectangle
-    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, thickness)
-
-    return (x_min, y_min), (x_max, y_max)
     
+    Parameters
+    ----------
+    frame : ndarray
+        The image on which to draw the rectangle.
+    
+    points : array_like
+        The list of (x, y) coordinates connecting each segment.
+    
+    color : tuple, optional
+        The color of the rectangle. The default color is (0, 0, 255).
+    
+    thickness : int, optional
+        The thickness of the rectangle. The default thickness is 1 px.
+    
+    Returns
+    ----------
+    The coordinates of the top left and bottom right corners of the rectangle, in the form (x_min, y_min), (x_max, y_max).
+    """
+    rectangle = geometry.get_min_enclosing_rectangle(vertices)
+    cv2.rectangle(frame, rectangle[0], rectangle[1], color, thickness)
+    return rectangle
+    
+
 # ------------------------------
 # EXAMPLE USAGE
 # ------------------------------
@@ -110,12 +269,16 @@ if __name__ == '__main__':
 
     vertices = []
     for i in range(100):
-        x = np.random.randint(0, 500)
-        y = np.random.randint(0, 500)
+        start, end = 100, 400
+
+        x = np.random.randint(start, end)
+        y = np.random.randint(start, end)
 
         vertices.append((x, y))
 
-    draw_points(frame, vertices, color=(255,0,0), length=5)
+    draw_all_crosses(frame, vertices, color=(255,255,255), length=5)
+    draw_polygon(frame, vertices, color=(255, 0,0))
+    draw_min_enclosing_rectangle(frame, vertices, color=(0, 0, 255))
     draw_convex_hull(frame, vertices, color=(0, 255, 0))
 
     while True:
