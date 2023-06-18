88405d086346f4a2ac5aa0ea55026b883872ce92
Merieme Bouisri <meriemebouisri@gmail.com>
2023-06-18 01:50:35 -0400

    Add imports

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from demos import *
 
-"""
-Call the demo functions here and run the program.
-"""
+import cv2
+import numpy as np
+import math
 
-iris_ellipse_demo()
+from eye_tracking.face_frame import FaceFrame
+from eye_tracking.face_mesh import RawFaceMesh, FaceNotFound
+from eye_tracking.iris_extraction import IrisEllipse, NoIrisFound, NoFittingEllipseFound
+from eye_tracking.eye_keypoints import eye_dict
 
+from eye_tracking.utils import draw_utils
+from eye_tracking.utils import image_utils
+from eye_tracking.utils import geometry_utils
 
+from demos import demo_fit_iris_ellipse, demo_camera_calibration
+from eye_tracking.calibration.calibrator import Calibrator
+from eye_tracking.calibration.camera_calibrator import CameraCalibrator
+
+# The following demo will show the iris ellipse fitting algorithm.
+# demo_fit_iris_ellipse.main()
+
+# The following demo will show the camera calibration algorithm.
+# demo_camera_calibration.main()
