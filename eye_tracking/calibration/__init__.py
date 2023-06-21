b56471f03b67f47f0bb2989b9ec883c1a843f0b9
Merieme Bouisri <meriemebouisri@gmail.com>
2023-06-21 09:20:42 -0400

    ADD - Calibration folder and logging

---
+++ eye_tracking/calibration/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('Calibration')
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+logger.setLevel(logging.DEBUG)
