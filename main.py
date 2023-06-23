ed3ec2cb0209d104455fc2949002651ff37fb42c
Merieme Bouisri <meriemebouisri@gmail.com>
2023-06-22 23:01:28 -0400

    Update imports and example

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-
 import cv2
 import numpy as np
 import math
-
-from eye_tracking.face_frame import FaceFrame
-from eye_tracking.face_mesh import RawFaceMesh, FaceNotFound
-from eye_tracking.iris_extraction import IrisEllipse, NoIrisFound, NoFittingEllipseFound
-from eye_tracking.eye_keypoints import eye_dict
+import os
 
 from eye_tracking.utils import draw_utils
 from eye_tracking.utils import image_utils
 from eye_tracking.utils import geometry_utils
 
-from demos import demo_fit_iris_ellipse, demo_camera_calibration
-from eye_tracking.calibration.calibrator import Calibrator
-from eye_tracking.calibration.camera_calibrator import CameraCalibrator
+from eye_tracking.face_frame import FaceFrame
+from eye_tracking.face_mesh import RawFaceMesh, FaceNotFound
+from eye_tracking.iris_ellipse_extractor import IrisEllipseExtractor
+from eye_tracking.corneal_reflection_extractor import CornealReflectionExtractor
 
-# The following demo will show the iris ellipse fitting algorithm.
-# demo_fit_iris_ellipse.main()
+from eye_tracking.eye_keypoints import eye_dict
 
-# The following demo will show the camera calibration algorithm.
-# demo_camera_calibration.main()
+from examples.example_pupil_reflection import example_corneal_reflection
+from examples.example_gaze_vector_2d import example_gaze_vector_2d
+
+example_gaze_vector_2d()
+
